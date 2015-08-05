b48de4409530ab72ea45600226f82787130b01a7
Brian Borealis & Julian Stewart <student@epicodus.com>
2015-08-05 11:55:24 -0700

    Update add.php and Task.php

--- app/app.php
+++ app/app.php
@@ -3,20 +3,31 @@
     require_once __DIR__."/../src/Task.php";
 
     session_start();
-    if (empty($SESSION['list_of_tasks'])) {
+    
+    if (empty($_SESSION['list_of_tasks'])) {
         $_SESSION['list_of_tasks'] = array();
     }
 
     $app = new Silex\Application();
 
     $app->get("/", function() {
+
         $output = "";
 
-        foreach (Task::getAll() as $task) {
-            $output = $output . "<p>" . $task->getDescription() . "</p>";
+        $all_tasks = Task::getAll();
+
+        if (!empty($all_tasks)) {
+            $output .= "
+                <h1>To Do List</h1>
+                <p>Here are all your tasks:</p>
+                ";
+
+            foreach ($all_tasks as $task) {
+                $output .= "<p>" . $task->getDescription() . "</p>";
+            }
         }
 
-        $output = $output . "
+        $output .= "
             <form action='/tasks' method='post'>
                 <label for='description'>Task Description</label>
                 <input id='description' name='description' type='text'>
@@ -25,7 +36,33 @@
             </form>
         ";
 
+        $output .= "
+            <form action='/delete_tasks' method='post'>
+                <button type='submit'>delete</button>
+            </form>
+        ";
+
         return $output;
+    });
+
+    $app->post("/tasks", function() {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return "
+                <h1>You created a task!</h1>
+                <p>" . $task->getDescription() . "</p>
+                <p><a href='/'>View your list of things to do.</a></p>
+        ";
+    });
+
+    $app->post("/delete_tasks", function() {
+
+        Task::deleteAll();
+
+        return "
+            <h1>List Cleared!</h1>
+            <p><a href='/''>Home</a></p>
+        ";
     });
 
     return $app;
