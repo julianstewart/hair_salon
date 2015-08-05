ba8d326dd89d7d3508d48c2f18a144fee39637c3
Brian Borealis & Julian Stewart <student@epicodus.com>
2015-08-05 10:40:46 -0700

    Update add.php and Task.php

--- app/app.php
+++ app/app.php
@@ -2,14 +2,14 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
 
+    session_start();
+    if (empty($SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        $test_task = new Task("Learn PHP.");
-        $another_test_task = new Task("Learn Drupal.");
-        $third_task = new Task("Visit France.");
-
-        $list_of_tasks = array($test_task, $another_test_task, $third_task);
         $output = "";
 
         foreach ($list_of_tasks as $task) {
