9f1890b57554e4b9c679f30cc18b5676c1548f59
Brian Borealis & Julian Stewart <student@epicodus.com>
2015-08-17 14:48:28 -0700

    all category tests passed

--- src/Category.php
+++ src/Category.php
@@ -28,6 +28,7 @@
         function save()
         {
             $GLOBALS['DB']->exec("INSERT INTO categories (name) VALUES ('{$this->getName()}')");
+            $this->id= $GLOBALS['DB']->lastInsertId();
         }
 
         static function getAll()
