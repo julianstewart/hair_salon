ff74c2a2d76b4f024fbb3f16b14658feba399b59
Julian Stewart <stewart.julian@gmail.com>
2015-08-21 16:33:30 -0700

    pass getStylistId test

--- src/Stylist.php
+++ src/Stylist.php
@@ -20,72 +20,15 @@
             return $this->stylist_name;
         }
 
-        function getId()
+        function getStylistId()
         {
-            return $this->id;
+            return $this->stylist_id;
         }
 
         function save()
         {
-            $GLOBALS['DB']->exec("INSERT INTO categories (name) VALUES ('{$this->getName()}')");
-            $this->id= $GLOBALS['DB']->lastInsertId();
-        }
-
-        function getTasks()
-        {
-            $tasks = Array();
-            $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks WHERE category_id = {$this->getId()}");
-            foreach($returned_tasks as $task) {
-                $description = $task['description'];
-                $id = $task['id'];
-                $category_id = $task['category_id'];
-                $new_task = new Task($description, $id, $category_id);
-                array_push($tasks, $new_task);
-            }
-            return $tasks;
-        }
-
-        function update($new_name)
-        {
-            $GLOBALS['DB']->exec("UPDATE categories SET name = '{$new_name}' WHERE id = {$this->getId()};");
-            $this->setName($new_name);
-        }
-
-        function delete()
-        {
-            $GLOBALS['DB']->exec("DELETE FROM categories WHERE id = {$this->getId()};");
-            $GLOBALS['DB']->exec("DELETE FROM tasks WHERE category_id = {$this->getId()};");
-        }
-
-        static function getAll()
-        {
-            $returned_categories = $GLOBALS['DB']->query("SELECT * FROM categories;");
-            $categories = array();
-            foreach($returned_categories as $category) {
-                $name = $category['name'];
-                $id = $category['id'];
-                $new_category = new Category($name, $id);
-                array_push($categories, $new_category);
-            }
-            return $categories;
-        }
-
-        static function deleteAll()
-        {
-            $GLOBALS['DB']->exec("DELETE FROM categories;");
-        }
-
-        static function find($search_id)
-        {
-            $found_category = null;
-            $categories = Category::getAll();
-            foreach($categories as $category) {
-                $category_id = $category->getId();
-                if ($category_id == $search_id) {
-                    $found_category = $category;
-                }
-            }
-            return $found_category;
+            $GLOBALS['DB']->exec("INSERT INTO stylists (name) VALUES ('{$this->getStylistName()}')");
+            $this->stylist_id= $GLOBALS['DB']->lastInsertStylistId();
         }
 
     }
